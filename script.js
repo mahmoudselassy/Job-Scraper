f543d37d11cf80157757d43cd02fb8e7a04865c4
Mahmoud Elassy <mahmoudsameh7331@yahoo.com>
2023-01-13 16:29:22 +0200

    scrape number of job postings

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const playwright = require("playwright");
 
-async function scrape() {
-    const browser = await playwright.chromium.launch({});
+const jobTitles = ["Back-End Developer", "Full Stack Developer", "Mobile Developer", "UI/UX Designer", "IT Specialist", "Software Engineer", "Database Administrator", "Data Analyst", "Front-End Developer", "Tester", "Network Engineer", "Cyber Security", "Machine Learning Engineer", "Embedded Systems Engineer", "Game Developer", "Data Engineer", "Dev-Ops"];
 
-    const page = await browser.newPage();
-    await page.goto("https://wuzzuf.net/search/jobs/?q=Backend+Engineer", {
-        waitUntil: "load",
-    });
-    /* await page.waitForSelector(
-        "#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2"
-    );*/
-    const num = await page.$eval(
-        "#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2 > span.css-xkh9ud > strong",
-        (el) => el.textContent
-    );
-    console.log(num);
-    await browser.close();
+async function scrape(jobTitle) {
+  const browser = await playwright.chromium.launch({});
+  const page = await browser.newPage();
+  const pageOptions = {
+    waitUntil: "load",
+    timeout: 0,
+  };
+  await page.goto(`https://wuzzuf.net/search/jobs/?q=${jobTitle}`, pageOptions);
+
+  const jobsNumber = await page.$eval("#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2 > span.css-xkh9ud > strong", (el) => el.textContent);
+  console.log(jobTitle, " : ", jobsNumber);
+  await browser.close();
 }
-scrape();
+jobTitles.forEach((jobTitle) => {
+  scrape(jobTitle);
+});
