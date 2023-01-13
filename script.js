1f552d06a4edcaa99e36967ff72d7188ab57612d
mahmoudelassy <mahmoudsameh7331@yahoo.com>
2023-01-13 19:05:56 +0200

    scrape job links for each page

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const playwright = require("playwright");
 
 const jobTitles = ["Back-End Developer", "Full Stack Developer", "Mobile Developer", "UI/UX Designer", "IT Specialist", "Software Engineer", "Database Administrator", "Data Analyst", "Front-End Developer", "Tester", "Network Engineer", "Cyber Security", "Machine Learning Engineer", "Embedded Systems Engineer", "Game Developer", "Data Engineer", "Dev-Ops"];
 
+const jobsNumberSelector = "#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2 > span.css-xkh9ud > strong";
+
+const jobPostLinkSelector = "#app > div > div.css-1omce3u > div > div > div:nth-child(2) > div > div > div.css-laomuu > h2 > a";
 async function scrape(jobTitle) {
   const browser = await playwright.chromium.launch({});
   const page = await browser.newPage();
@@ -9,12 +12,22 @@ async function scrape(jobTitle) {
     waitUntil: "load",
     timeout: 0,
   };
-  await page.goto(`https://wuzzuf.net/search/jobs/?q=${jobTitle}`, pageOptions);
+  const url = `https://wuzzuf.net/search/jobs/?q=${jobTitle}`;
+  await page.goto(url, pageOptions);
 
-  const jobsNumber = await page.$eval("#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2 > span.css-xkh9ud > strong", (el) => el.textContent);
-  console.log(jobTitle, " : ", jobsNumber);
-  await browser.close();
+  const jobsNumber = Number(await page.$eval(jobsNumberSelector, (el) => el.textContent));
+  const NumberOfPages = Math.ceil(jobsNumber / 15);
+  for (let i = 0; i < NumberOfPages; i++) {
+    await page.goto(`${url}&start=${i}`, pageOptions);
+    const jobsLinks = await page.$$(jobPostLinkSelector);
+    for (const jobsLink of jobsLinks) {
+      console.log(await (await jobsLink.getProperty("href")).jsonValue());
+    }
+  }
+  browser.close();
 }
+/*
 jobTitles.forEach((jobTitle) => {
   scrape(jobTitle);
-});
+});*/
+scrape("Back-End Developer");
