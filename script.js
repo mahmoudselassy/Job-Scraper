24033f1ed5d84e641e062b12f10da1eee120a174
mahmoudelassy <mahmoudsameh7331@yahoo.com>
2023-01-13 21:42:56 +0200

    add jobsNumber from loaded html

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const playwright = require("playwright");
+const cheerio = require("cheerio");
 
 const jobTitles = ["Back-End Developer", "Full Stack Developer", "Mobile Developer", "UI/UX Designer", "IT Specialist", "Software Engineer", "Database Administrator", "Data Analyst", "Front-End Developer", "Tester", "Network Engineer", "Cyber Security", "Machine Learning Engineer", "Embedded Systems Engineer", "Game Developer", "Data Engineer", "Dev-Ops"];
 
 const jobsNumberSelector = "#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2 > span.css-xkh9ud > strong";
 
 const jobPostLinkSelector = "#app > div > div.css-1omce3u > div > div > div:nth-child(2) > div > div > div.css-laomuu > h2 > a";
+
 async function scrape(jobTitle) {
   const browser = await playwright.chromium.launch({});
   const page = await browser.newPage();
@@ -14,8 +16,11 @@ async function scrape(jobTitle) {
   };
   const url = `https://wuzzuf.net/search/jobs/?q=${jobTitle}`;
   await page.goto(url, pageOptions);
+  const html = await page.evaluate(() => document.body.innerHTML);
+  const $ = cheerio.load(html);
+  const jobsNumber = Number($(jobsNumberSelector).text());
 
-  const jobsNumber = Number(await page.$eval(jobsNumberSelector, (el) => el.textContent));
+  /* 
   const NumberOfPages = Math.ceil(jobsNumber / 15);
   for (let i = 0; i < NumberOfPages; i++) {
     await page.goto(`${url}&start=${i}`, pageOptions);
@@ -23,7 +28,7 @@ async function scrape(jobTitle) {
     for (const jobsLink of jobsLinks) {
       console.log(await (await jobsLink.getProperty("href")).jsonValue());
     }
-  }
+  }*/
   browser.close();
 }
 /*
