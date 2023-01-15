7bea23435f1a0bb1380c3533b2efc21096897e7b
mahmoudelassy <mahmoudsameh7331@yahoo.com>
2023-01-16 00:45:06 +0200

    scrape job description and job requirments

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const playwright = require("playwright");
+const fs = require("fs");
+const csv = require("csv-stringify");
 const { parse } = require("node-html-parser");
 
-const jobTitles = ["Back-End Developer", "Full Stack Developer", "Mobile Developer", "UI/UX Designer", "IT Specialist", "Software Engineer", "Database Administrator", "Data Analyst", "Front-End Developer", "Tester", "Network Engineer", "Cyber Security", "Machine Learning Engineer", "Embedded Systems Engineer", "Game Developer", "Data Engineer", "Dev-Ops"];
+//const jobTitles = ["Back-End Developer", "Full Stack Developer", "Mobile Developer", "UI UX Designer", "IT Specialist", "Software Engineer", "Database Administrator", "Data Analyst", "Front-End Developer", "Tester", "Network Engineer", "Cyber Security", "Machine Learning Engineer", "Embedded Systems Engineer", "Game Developer", "Data Engineer", "Dev-Ops"];
+
+const jobTitles = ["Cyber Security", "Dev-Ops"];
 
 const jobsNumberSelector = "#app > div > div.css-1omce3u > div > div > div.css-13hf9up.e1v1l3u10 > div.css-osele2 > span.css-xkh9ud > strong";
 
@@ -17,6 +21,10 @@ const skillsSelector = "#app > div > main > article > section.css-3kx5e2 > div.c
 
 const experienceSelector = "#app > div > main > article > section.css-3kx5e2 > div:nth-child(2) > span.css-47jx3m > span";
 
+const jobDescriptionSelector = "#app > div > main > article > section:nth-child(4)";
+
+const jobRequirementsSelector = "#app > div > main > article > section:nth-child(5)";
+
 let browser;
 
 const loadHtml = async (url) => {
@@ -25,7 +33,15 @@ const loadHtml = async (url) => {
     timeout: 0,
   };
   const page = await browser.newPage();
-  await page.goto(url, pageOptions);
+  while (true) {
+    try {
+      await page.goto(url, pageOptions);
+    } catch (e) {
+      console.log(e);
+      continue;
+    }
+    break;
+  }
   let html = await page.evaluate(() => document.body.innerHTML);
   page.close();
   return parse(html);
@@ -34,37 +50,57 @@ const loadHtml = async (url) => {
 const scrapeJobPage = async (jobLink) => {
   const jobPage = await loadHtml(`https://wuzzuf.net${jobLink.getAttribute("href")}`);
   const job = {
-    job_title: jobLink.innerHTML,
+    job_title: jobLink ? jobLink.innerHTML : "",
     company: jobPage.querySelector(companySelector) ? jobPage.querySelector(companySelector).innerText : "",
-    city: jobPage.querySelector(locationSelector)?.innerText.split(";")[1],
-    from: jobPage.querySelector(fromSelector).innerText,
+    city: jobPage.querySelector(locationSelector) ? jobPage.querySelector(locationSelector).innerText.split(";")[1] : "",
+    from: jobPage.querySelector(fromSelector) ? jobPage.querySelector(fromSelector).innerText : "",
     skills: [],
-    YearsOfExperience: jobPage.querySelector(experienceSelector).innerText,
-    link: `https://wuzzuf.net${jobLink.getAttribute("href")}`,
+    YearsOfExperience: jobPage.querySelector(experienceSelector) ? jobPage.querySelector(experienceSelector).innerText : "",
+    JobDescription: jobPage.querySelector(jobDescriptionSelector) ? jobPage.querySelector(jobDescriptionSelector).textContent : "",
+    JobRequirements: jobPage.querySelector(jobRequirementsSelector) ? jobPage.querySelector(jobRequirementsSelector).textContent : "",
+    link: `https://wuzzuf.net${jobLink.getAttribute("href")}`.split("?")[0],
   };
   jobPage.querySelectorAll(skillsSelector).forEach((skill, index) => job.skills.push(skill.innerText));
   job.skills = job.skills.join(",");
-  console.log(job);
+  return job;
 };
 
-async function scrape(jobTitle) {
+const scrape = async (jobTitle) => {
+  fs.writeFileSync(`./scraped_data/${jobTitle}.csv`, "");
+  csv.stringify(
+    [],
+    {
+      header: true,
+      columns: ["job_title", "company", "city", "from", "skills", "YearsOfExperience", "JobDescription", "JobRequirements", "link"],
+    },
+    (err, output) => fs.appendFileSync(`./scraped_data/${jobTitle}.csv`, output)
+  );
   browser = await playwright.chromium.launch();
   const url = `https://wuzzuf.net/search/jobs/?q=${jobTitle}`;
   let searchPage = await loadHtml(url);
-  const jobsNumber = Number(searchPage.querySelector(jobsNumberSelector).innerText);
-  console.log(jobsNumber);
+  const jobsNumber = Number(searchPage.querySelector(jobsNumberSelector).innerText.replace(",", ""));
   const NumberOfPages = Math.ceil(jobsNumber / 15);
-  for (let i = 0; i < 1; i++) {
+  console.log(jobTitle, ":", jobsNumber);
+  for (let i = 0; i < NumberOfPages; i++) {
     searchPage = await loadHtml(`${url}&start=${i}`);
     const jobsLinks = searchPage.querySelectorAll(jobPostLinkSelector);
     for (const jobLink of jobsLinks) {
-      await scrapeJobPage(jobLink);
+      const job = await scrapeJobPage(jobLink);
+      csv.stringify([job], (err, output) => fs.appendFileSync(`./scraped_data/${jobTitle}.csv`, output));
     }
   }
-  browser.close();
-}
-/*
-jobTitles.forEach((jobTitle) => {
-  scrape(jobTitle);
-});*/
-scrape("Back-End Developer");
+  if (browser.isConnected()) {
+    await browser.close();
+  }
+};
+
+(async () => {
+  for (const jobTitle of jobTitles) {
+    console.log("start:", jobTitle);
+    await scrape(jobTitle);
+    console.log("end:", jobTitle);
+    if (browser.isConnected()) {
+      await browser.close();
+    }
+  }
+})();
